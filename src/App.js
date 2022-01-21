75ef7d50b8ad26e09042c5052f0cc9f144f9c103
Carlos Martinez <curlosmart@gmail.com>
2022-01-21 11:21:44 -0500

    setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Box } from "@chakra-ui/react"
+import Navbar from "./components/Navbar";
+
+
+const App = () => {
+  return (
+    <Box bgColor="#070724" minHeight="100vh" color="#FFFFFF" fontFamily="Antonio">
+      <Navbar />
+    </Box>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { ChakraProvider } from '@chakra-ui/react'
+
+ReactDOM.render(
+  <ChakraProvider resetCSS="true">
+    <App />
+  </ChakraProvider>,
+  document.getElementById('root')
+);
