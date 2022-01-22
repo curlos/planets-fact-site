ad500fcc34b6670d2624d44eb103cc2ef1bf7054
Carlos Martinez <curlosmart@gmail.com>
2022-01-21 22:44:40 -0500

    added responsiveness for mobile

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,18 @@
 import React from 'react';
-import { Box, Flex, Text, Image } from "@chakra-ui/react"
+import { Box, Flex, Text, Image, textDecoration } from "@chakra-ui/react"
 import getData from '../utils/data'
 import { Link } from 'react-router-dom';
+
+const COLORS = {
+  'Mercury': '#419EBB',
+  'Venus': '#EDA249',
+  'Earth': '#6D2ED5',
+  'Mars': '#D14C32',
+  'Jupiter': '#D83A34',
+  'Saturn': '#CD5120',
+  'Uranus': '#1EC1A2',
+  'Neptune': '#2D68F0'
+}
 
 const Navbar = () => {
 
@@ -10,15 +21,15 @@ const Navbar = () => {
   console.log(planets)
 
   return (
-    <Flex justify="space-between" align="center" p="20px" borderBottom="1px" borderColor="#838391" position="sticky">
+    <Flex flexDirection={{md: 'column', lg: 'row'}} justify="space-between" align="center" gap={{ md: '20px' }} p="20px" borderBottom="1px" borderColor="#838391" position="sticky">
       <Link to="/">
         <Box fontSize="28px">THE PLANETS</Box>
       </Link>
 
-      <Flex align="center" gap="14px" fontFamily="Spartan" fontWeight="700" fontSize="11px" lineHeight="25px" letterSpacing={"1px"} bgColor={{ base: 'white', sm: 'pink', md: 'red', xl: 'blue'}} d={{ base: 'none', md: 'flex'}}>
+      <Flex align="center" gap="14px" fontFamily="Spartan" fontWeight="700" fontSize="11px" lineHeight="25px" letterSpacing={"1px"} d={{ base: 'none', md: 'flex'}}>
         {planets.map((planet) => (
           <Link to={`/planet/${planet.name}`}>
-            <Text cursor="pointer">{planet.name}</Text>
+            <Text cursor="pointer" borderBottom="1px" borderColor="transparent" _hover={{ color: COLORS[planet.name], borderColor: COLORS[planet.name] }}>{planet.name}</Text>
           </Link>
         ))}
       </Flex>
--- src/pages/Planet.js
+++ src/pages/Planet.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Box, Flex, Grid, Image, Text, Link, Button } from "@chakra-ui/react"
+import { Box, Flex, Grid, Image, Text, Link, VStack } from "@chakra-ui/react"
 import { useParams } from 'react-router-dom';
 import getData from '../utils/data';
 
@@ -12,7 +12,6 @@ const COLORS = {
   'Saturn': '#CD5120',
   'Uranus': '#1EC1A2',
   'Neptune': '#2D68F0'
-
 }
 
 const Planet = () => {
@@ -20,6 +19,12 @@ const Planet = () => {
   const { name } = useParams()
   const planets = getData()
   const planet = planets.find((planet) => planet.name === name)
+  const planetStats = {
+    'ROTATION TIME': planet.rotation,
+    'REVOLUTION TIME': planet.revolution,
+    'RADIUS': planet.radius,
+    'AVERAGE TEMP.': planet.temperature
+  }
   const [type, setType] = useState('planet')
 
   useEffect(() => {
@@ -32,7 +37,7 @@ const Planet = () => {
 
   return (
     <Box p="30px">
-      <Grid templateColumns="2fr 1fr" d={{ base: 'block', md: 'grid'}}>
+      <Grid templateColumns={{ base: 'none', lg: '2fr 1fr' }} templateRows={{ md: '2fr 1fr', lg: 'none'}}>
         {type !== 'geology' ? (
           <Flex justify="center" align="center" flexGrow="2">
             <Image src={planet.images[type].slice(1,)} height={{ base: '200px', md: '336px'}} mb={{ base: '20px', md: '0px' }}/>
@@ -43,16 +48,18 @@ const Planet = () => {
             <Image src={planet.images[type].slice(1,)} height="199px" mb="-330px" position="absolute"/>
           </Flex>
         )}
-        <Flex flexDirection="column" gap="20px" flexGrow="1">
-          <Text fontSize="80px" textAlign={{ base: 'center', md: 'start'}}>{planet.name}</Text>
-          <Text fontFamily="Spartan" fontSize="14px" textAlign={{ base: 'center', md: 'start'}}>
-            {planet.overview.content}
-          </Text>
-          <Text fontFamily="Spartan" fontSize="14px" color="#838391" textAlign={{ base: 'center', md: 'start'}}>
-            Source: <Link href={planet.overview.content} textDecoration="underline" color="#FFFFFF">Wikipedia.org</Link>
-          </Text>
+        <Flex flexDirection={{base: 'row', lg: 'column'}} gap="20px" d={{ base: 'block', md: 'flex'}}>
+          <VStack spacing={{ base: '20px' }} align={{ md: 'start' }} mb={{ base: '20px', md: '0px', 'lg': '20px' }} width={{ md: '55%', lg: 'auto' }} mr={{ md: '30px', lg: '0px' }}>
+            <Text fontSize="80px" textAlign={{ base: 'center', md: 'start'}}>{planet.name}</Text>
+            <Text fontFamily="Spartan" fontSize="14px" textAlign={{ base: 'center', md: 'start'}}>
+              {planet.overview.content}
+            </Text>
+            <Text fontFamily="Spartan" fontSize="14px" color="#838391" textAlign={{ base: 'center', md: 'start'}}>
+              Source: <Link href={planet.overview.content} textDecoration="underline" color="#FFFFFF">Wikipedia.org</Link>
+            </Text>
+          </VStack>
 
-          <Flex flexDirection="column" gap="20px">
+          <Flex flexDirection="column" justify="center" gap="20px" width={{ md: '45%', lg: 'auto' }}>
             <Box d="flex" gap="25px" p="20px" px="25px" m="0px" bgColor={type === 'planet' ? COLORS[planet.name] : '#070724'} border={'1px'} borderColor={type !== 'planet' ? '#838391' : 'transparent'} borderRadius="0px" fontFamily="Spartan" fontSize="12px" fontWeight="bold" letterSpacing="2px" cursor="pointer" _hover={{ bgColor: '#38384F', borderColor: '#38384F' }} onClick={() => setType('planet')}>
               <Text>01</Text>
               <Text>OVERVIEW</Text>
@@ -66,34 +73,18 @@ const Planet = () => {
             <Box d="flex" gap="25px" p="20px" px="25px" m="0px" bgColor={type === 'geology' ? COLORS[planet.name] : '#070724'} border={'1px'} borderColor={type !== 'geology' ? '#838391' : 'transparent'} borderRadius="0px" fontFamily="Spartan" fontSize="12px" fontWeight="bold" letterSpacing="2px" cursor="pointer" _hover={{ bgColor: '#38384F', borderColor: '#38384F' }} onClick={() => setType('geology')}>
               <Text>03</Text>
               <Text>SURFACE GEOLOGY</Text>
-            </Box>
-
-            
+            </Box> 
           </Flex>
         </Flex>
       </Grid>
 
-      <Grid templateColumns="1fr 1fr 1fr 1fr" gridGap="20px" py="30px" px={{ md: '70px'}} my="30px" d={{ base: 'block', md: 'grid'}}>
-        <Box border="1px" borderColor="#838391" p="20px" mb={{ base: '15px', md: '0px'}}>
-          <Text fontSize="11px" fontWeight="bold" fontFamily="Spartan" color="#838391">ROTATION TIME</Text>
-          <Text fontSize="40px" fontWeight="medium">{planet.rotation}</Text>
-        </Box>
-
-        <Box border="1px" borderColor="#838391" p="20px" mb={{ base: '15px', md: '0px'}}>
-          <Text fontSize="11px" fontWeight="bold" fontFamily="Spartan" color="#838391">REVOLUTION TIME</Text>
-          <Text fontSize="40px" fontWeight="medium">{planet.revolution}</Text>
-        </Box>
-
-        <Box border="1px" borderColor="#838391" p="20px" mb={{ base: '15px', md: '0px'}}>
-          <Text fontSize="11px" fontWeight="bold" fontFamily="Spartan" color="#838391">RADIUS</Text>
-          <Text fontSize="40px" fontWeight="medium">{planet.radius}</Text>
-        </Box>
-
-        <Box border="1px" borderColor="#838391" p="20px" mb={{ base: '15px', md: '0px'}}>
-          <Text fontSize="11px" fontWeight="bold" fontFamily="Spartan" color="#838391">AVERAGE TEMP</Text>
-          <Text fontSize="40px" fontWeight="medium">{planet.temperature}</Text>
-        </Box>
-        
+      <Grid templateColumns="1fr 1fr 1fr 1fr" gridGap="20px" py="30px" px={{ lg: '70px'}} my="30px" d={{ base: 'block', md: 'grid'}}>
+        {Object.keys(planetStats).map((statName) => (
+          <Flex d={{ base: 'flex', md: 'block'}} justify="space-between" align="center" width="100%" border="1px" borderColor="#838391" p="20px" mb={{ base: '15px', md: '0px'}}>
+            <Text fontSize={{ base: '8px', xl: '11px'}}  fontWeight="bold" fontFamily="Spartan" color="#838391">{statName}</Text>
+            <Text fontSize={{ base: '20px', md: '24px', xl: '40px'}} fontWeight="medium">{planetStats[statName]}</Text>
+          </Flex>
+        ))}
       </Grid>
     </Box>
   );
